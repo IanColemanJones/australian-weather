67b38d50e6e29796fa1951b5ecc355b98cdfa092
IanColemanJones <i.jones.018@gmail.com>
2021-09-23 11:42:16 +0100

    Changes from ex4

--- R/create-plot.R
+++ R/create-plot.R
@@ -11,14 +11,24 @@ df = read_csv("data/weatherAUS.csv")
 
 city = "Sydney"
 
+time_series =
+  df %>%
+  filter(Location == city) %>%
+  ggplot(aes(x = Date, y = Temp9am)) +
+  geom_line()
+ggsave(filename = "graphics/timeseries-rainfall.png",
+       plot = time_series)
+
 scatter_plot =
   df %>%
-  filter(Location == "Brisbane") %>%
+  filter(Location == "Sydney") %>%
   ggplot(aes(x = Temp9am, y = Humidity9am)) +
   geom_point(colour = "red") +
-  labs(title = "Humidity and Temperatur for Brisbane") +
+  labs(title = "Humidity and Temperatur for Sydney") +
   theme_minimal() +
   facet_wrap(~WindDir9am)
 
 # Save the plot in graphics/
+ggsave(filename = "graphics/scatter-temp-humidity.png", plot = time_series)
+
 ggsave(filename = "graphics/scatter-temp-humidity.png", plot = scatter_plot)
